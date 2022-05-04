bf8d847e079c80e418593114799e9ff50e2ed02f
Dillon Lucas <dill17lion@gmail.com>
2022-05-04 13:04:53 -0500

    Initial Commit

---
+++ Shared/DesignCodeChallengeOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeChallengeOneApp.swift
+//  Shared
+//
+//  Created by Dillon Lucas on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeChallengeOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
