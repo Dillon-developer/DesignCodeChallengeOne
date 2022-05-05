c7eac7b65b91264f8a278c34f86de5a6ba7a32b3
Dillon Lucas <dill17lion@gmail.com>
2022-05-05 12:41:22 -0500

    update

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,11 +10,16 @@ import SwiftUI
 struct ContentView: View {
     
     @State var show = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
             
             TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default, value: show)
+            
+            BottomCardView()
                 .blur(radius: show ? 20 : 0)
                 .animation(.default, value: show)
             
@@ -24,10 +29,12 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.9)
                 .rotationEffect(.degrees(show ? 0 : 10))
                 .rotation3DEffect(.degrees(10), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5), value: viewState)
                 .animation(.easeInOut(duration: 0.5), value: show)
             
             BackCardView()
@@ -35,24 +42,38 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
                 .rotationEffect(.degrees(show ? 0 : 5))
                 .rotation3DEffect(.degrees(5), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
-                .animation(.easeInOut(duration: 0.4), value: show)
+                .animation(.easeInOut(duration: 0.3), value: viewState)
+                .animation(.easeInOut(duration: 0.3), value: show)
                             
             CardView()
+                .offset(x: viewState.width, y: viewState.height)
                 .blendMode(.hardLight)
+                .animation(.spring(response: 0.2, dampingFraction: 0.6, blendDuration: 0), value: viewState)
                 .onTapGesture { self.show.toggle() }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                        //self.show = true
+                        
+                    }
+                    .onEnded {value in
+                        self.viewState = .zero
+                    }
+                )
                     
                     
-                }
-            
-            BottomCardView()
-                .blur(radius: show ? 20 : 0)
-                .animation(.default, value: show)
                 
+            
+            
+        }.onAppear {
+            print(show)
         }
+      }
             
         
     }
