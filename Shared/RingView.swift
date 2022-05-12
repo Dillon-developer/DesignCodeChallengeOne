26bc2124d459a619e371e1d9d6d5d4a560738b19
Dillon Lucas <dill17lion@gmail.com>
2022-05-12 09:52:01 -0500

    23 / 40

--- Shared/RingView.swift
+++ Shared/RingView.swift
@@ -8,31 +8,42 @@
 import SwiftUI
 
 struct RingView: View {
+    var color1 = Color.green
+    var color2 = Color.cyan
+    var color3 = Color.blue
+    var width: CGFloat = 88
+    var height: CGFloat = 88
+    var percent: CGFloat = 67
+    
+    
     var body: some View {
-        ZStack {
+        let multiplier = width / 44
+        let progress = 1 - percent / 100
+        
+        return ZStack {
             Circle()
                 .stroke(
                     .black.opacity(0.1),
-                    style: StrokeStyle(lineWidth: 8)
+                    style: StrokeStyle(lineWidth: 8 * multiplier)
                 )
-                .frame(width: 44, height: 44)
+                .frame(width: width, height: height)
             
             
             
             Circle()
-                .trim(from: 0.3, to: 1)
+                .trim(from: progress, to: 1)
                 .stroke(
-                    LinearGradient(gradient: Gradient(colors: [.green, .cyan, .blue]), startPoint: .topTrailing, endPoint: .bottomLeading),
-                    style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                    LinearGradient(gradient: Gradient(colors: [color1, color2, color3]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
                 )
                 .rotationEffect(.degrees(90))
                 .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
-                .frame(width: 44, height: 44)
-            .shadow(color: .blue.opacity(0.1), radius: 3, x: 0, y: 3)
+                .frame(width: width, height: height)
+            .shadow(color: .blue.opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
             
             
-            Text("82%")
-                .font(.subheadline)
+            Text("\(Int(percent))%")
+                .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
         }
     }
